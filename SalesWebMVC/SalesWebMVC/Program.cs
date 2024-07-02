eaa82a4cb1e8ef07a19e38c2d29bed45fa5a390e
Marco Antonio Casanova <masucasanova@gmail.com>
2024-07-02 14:30:12 -0300

    Feat CRUD
    CRUD scaffolding

--- SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMVC.Data;
 namespace SalesWebMVC;
 
 public class Program
@@ -5,6 +8,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
